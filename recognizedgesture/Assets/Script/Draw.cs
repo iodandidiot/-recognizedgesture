b7dbc3a38598dd4654d314d8c76c11454312b90e
Oles <iodandidiot@gmail.com>
2015-10-09 18:52:33 +0300

    first commit

---
+++ recognizedgesture/Assets/Script/Draw.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Draw : MonoBehaviour
+{
+
+    // Use this for initialization
+    private LineRenderer line;
+    private bool isMousePressed;
+    public List<Vector2> pointsList;
+    private Vector2 mousePos;
+    public GameObject template;
+    struct myLine
+    {
+        public Vector3 StartPoint;
+        public Vector3 EndPoint;
+    };
+
+    void Awake()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.SetVertexCount(0);
+        line.SetColors(Color.green, Color.green);
+        line.useWorldSpace = true;
+        isMousePressed = false;
+        pointsList = new List<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMousePressed = true;
+            line.SetVertexCount(0);
+            pointsList.RemoveRange(0, pointsList.Count);
+            line.SetColors(new Color(1, 0, 0), Color.green);
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMousePressed = false;
+            //line.SetColors(new Color(1, 0, 0), new Color(1, 0, 0, 0));
+        }
+        if (isMousePressed)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //mousePos.z = 0;
+            if (!pointsList.Contains(mousePos))
+            {
+                //if (pointsList.Count > 4) pointsList.RemoveRange(0,1);
+                pointsList.Add(mousePos);
+                
+                line.SetVertexCount(pointsList.Count);
+                line.SetPosition(pointsList.Count - 1, (Vector3)pointsList[pointsList.Count - 1]);
+            }
+            if (isLineCollide ()) 
+            {
+                    isMousePressed = false;
+                    line.SetColors (Color.red, Color.red);
+                    AddPoligon();
+            }
+        }
+    }
+    private bool isLineCollide ()
+    {
+        if (pointsList.Count < 2) return false;
+        int TotalLines = pointsList.Count - 1;
+        myLine[] lines = new myLine[TotalLines];
+        if (TotalLines > 1) {
+            for (int i=0; i<TotalLines; i++) {
+                lines [i].StartPoint = (Vector3)pointsList [i];
+                lines [i].EndPoint = (Vector3)pointsList [i + 1];
+            }
+        }
+        for (int i=0; i<TotalLines-1; i++) {
+            myLine currentLine;
+            currentLine.StartPoint = (Vector3)pointsList [pointsList.Count - 2];
+            currentLine.EndPoint = (Vector3)pointsList [pointsList.Count - 1];
+            if (isLinesIntersect (lines [i], currentLine))
+                return true;
+        }
+        return false;
+    }
+
+    private bool checkPoints (Vector3 pointA, Vector3 pointB)
+    {
+        return (pointA.x == pointB.x && pointA.y == pointB.y);
+    }
+    
+    private bool isLinesIntersect (myLine L1, myLine L2)
+    {
+        if (checkPoints (L1.StartPoint, L2.StartPoint) ||
+            checkPoints (L1.StartPoint, L2.EndPoint) ||
+            checkPoints (L1.EndPoint, L2.StartPoint) ||
+            checkPoints (L1.EndPoint, L2.EndPoint))
+            return false;
+        
+        return((Mathf.Max (L1.StartPoint.x, L1.EndPoint.x) >= Mathf.Min (L2.StartPoint.x, L2.EndPoint.x)) &&
+            (Mathf.Max (L2.StartPoint.x, L2.EndPoint.x) >= Mathf.Min (L1.StartPoint.x, L1.EndPoint.x)) &&
+            (Mathf.Max (L1.StartPoint.y, L1.EndPoint.y) >= Mathf.Min (L2.StartPoint.y, L2.EndPoint.y)) &&
+            (Mathf.Max (L2.StartPoint.y, L2.EndPoint.y) >= Mathf.Min (L1.StartPoint.y, L1.EndPoint.y))
+         );
+    }
+    void AddPoligon()
+    {
+        Template goToPoligon = template.GetComponent<Template>();
+        goToPoligon.checkPoligon(pointsList.ToArray());
+    }
+}
---
+++ recognizedgesture/Assets/Script/Template.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Template : MonoBehaviour
+{
+    LineRenderer line;
+    PolygonCollider2D coll;
+    Vector2[] position;
+    public GameObject checkSprite;
+    Vector2 centrMass;
+    // Use this for initialization
+    void Start()
+    {
+        line = transform.GetComponent<LineRenderer>();
+        line.SetVertexCount(5);
+        line.SetPosition(0, new Vector3(-1, 1, 0));
+        line.SetPosition(1, new Vector3(1, 1, 0));
+        line.SetPosition(2, new Vector3(1, -1, 0));
+        line.SetPosition(3, new Vector3(-1, -1, 0));
+        line.SetPosition(4, new Vector3(-1, 1, 0));
+        position = new Vector2[10];
+        position[0] = new Vector2(-1, 1);
+        position[1] = new Vector2(1, 1);
+        position[2] = new Vector2(1, -1);
+        position[3] = new Vector2(-1, -1);
+        position[4] = new Vector2(-1, 1);
+        position[5] = new Vector2(-1.1f, 1.1f);
+        position[6] = new Vector2(1.1f, 1.1f);
+        position[7] = new Vector2(1.1f, -1.1f);
+        position[8] = new Vector2(-1.1f, -1.1f);
+        position[9] = new Vector2(-1.1f, 1.1f);
+        coll = gameObject.AddComponent<PolygonCollider2D>();
+
+        //ollgameObject.GetComponent<PolygonCollider2D>().SetPath(0, position);
+        coll.SetPath(0, position);
+        //Rigidbody2D __rigidbody = gameObject.AddComponent<Rigidbody2D>();
+        //__rigidbody.isKinematic = true;
+        //centrMass = __rigidbody.worldCenterOfMass;
+        centrMass = coll.bounds.center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(coll.bounds);
+    }
+
+
+    public void checkPoligon(Vector2[] Array)//находим расстояние от начальной точки и сдвигаем весь полигон на это расстояние
+    {
+        float checkPosX = position[0].x - Array[0].x;
+        float checkPosY = position[0].y - Array[0].y;
+        PolygonCollider2D cSC = checkSprite.gameObject.AddComponent<PolygonCollider2D>();
+        //for (int i = 0; i < Array.Length; i++)
+        //{
+        //    Array[i].x += checkPosX-1;
+        //    Array[i].y += checkPosY-1;
+        //}
+        cSC.SetPath(0, Array);
+        //Rigidbody2D __spriteRigidbody = checkSprite.gameObject.AddComponent<Rigidbody2D>();
+        //__spriteRigidbody.isKinematic = true;
+        //__spriteRigidbody.centerOfMass = centrMass;
+        Vector2 poligonCentreMass = cSC.bounds.center;
+        Vector2 chengePos=poligonCentreMass - centrMass;
+        print(cSC.bounds.size);
+        
+        for (int i = 0; i < Array.Length; i++)
+        {
+            Array[i] -= chengePos;
+
+        }
+
+        cSC.SetPath(0, Array);
+        checkSprite.gameObject.transform.localScale = new Vector2((coll.bounds.size.x / cSC.bounds.size.x+coll.bounds.size.y / cSC.bounds.size.y)/2,
+        (coll.bounds.size.x / cSC.bounds.size.x+coll.bounds.size.y / cSC.bounds.size.y)/2);
+        print(cSC.bounds.size);
+        int count = 0;
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (coll.bounds.ClosestPoint(cSC.points[i]).x == cSC.points[i].x && coll.bounds.ClosestPoint(cSC.points[i]).y == cSC.points[i].y)
+            {
+                count += 1;
+            }
+        }
+        print(count);
+    }
+}
