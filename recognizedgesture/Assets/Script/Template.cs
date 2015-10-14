97b829ade118ad60fba15edf666bc5bf96a7257c
Oles <iodandidiot@gmail.com>
2015-10-14 12:35:04 +0300

    правки добавления искомой фигуры

--- recognizedgesture/Assets/Script/Template.cs
+++ recognizedgesture/Assets/Script/Template.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Template : MonoBehaviour
 {
-    LineRenderer line;
-    PolygonCollider2D coll;
+    LineRenderer line;//визуализация заданного полигона
+    PolygonCollider2D coll;//заданный полигон
+    PolygonCollider2D cell;
     Vector2[] position;
     public GameObject checkSprite;
     Vector2 centrMass;
     Vector2 poligonCentreMass;
     Rigidbody2D rgbody;
+    public int countPolig=3;
     // Use this for initialization
     void Start()
     {
@@ -34,7 +36,7 @@ public class Template : MonoBehaviour
         //coll = gameObject.AddComponent<PolygonCollider2D>();
         //coll.SetPath(0, position);        
         //centrMass = coll.bounds.center;
-        startFigur(5);
+        startFigur(countPolig);
     }
 
     // Update is called once per frame
@@ -69,7 +71,7 @@ public class Template : MonoBehaviour
         int count = 0;
         for (int i = 0; i < Array.Length; i++)
         {
-            if (coll.OverlapPoint(Array[i]))
+            if (cell.OverlapPoint(Array[i]))
             {
                 //print(Array[i]);
                 count += 1;
@@ -81,45 +83,67 @@ public class Template : MonoBehaviour
     }
 
 
-    void checkPoints(int reyCount)
-    {
-        float _x=0;
-        float _y=0;
-        float Angle = 360 * Mathf.Deg2Rad;
-        for (int i = 0; i < reyCount; i++)
-        {
-            _x = Mathf.Cos(Angle / reyCount * i)*10;
-            _y = Mathf.Cos(Angle / reyCount * i)*10;
-            print(new Vector2(_x, _y));
-            RaycastHit2D hit = Physics2D.Raycast(centrMass, new Vector2(_x, _y));
-            print(hit.collider.name);
-            CheckDistance(hit.point, _x, _y);
-        }
-    }
+    //void checkPoints(int reyCount)
+    //{
+    //    float _x=0;
+    //    float _y=0;
+    //    float Angle = 360 * Mathf.Deg2Rad;
+    //    for (int i = 0; i < reyCount; i++)
+    //    {
+    //        _x = Mathf.Cos(Angle / reyCount * i)*10;
+    //        _y = Mathf.Cos(Angle / reyCount * i)*10;
+    //        print(new Vector2(_x, _y));
+    //        RaycastHit2D hit = Physics2D.Raycast(centrMass, new Vector2(_x, _y));
+    //        print(hit.collider.name);
+    //        CheckDistance(hit.point, _x, _y);
+    //    }
+    //}
 
-    float CheckDistance(Vector2 hintpos,float _x,float _y)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(centrMass, new Vector2(_x, _y));
-        print(hit.collider.name);
-        return hit.distance;
-    }
+    //float CheckDistance(Vector2 hintpos,float _x,float _y)
+    //{
+    //    RaycastHit2D hit = Physics2D.Raycast(centrMass, new Vector2(_x, _y));
+    //    print(hit.collider.name);
+    //    return hit.distance;
+    //}
     void startFigur(int kolStor)
     {
+        float _var=1f;
         float Angle=360;
         Angle = Angle * Mathf.Deg2Rad;
         float checkAngle=Angle/kolStor;
         //PolygonCollider2D poligon = gameObject.AddComponent<PolygonCollider2D>();
-        Vector2 [] poligonPoints=new Vector2[kolStor+1];
+        position = new Vector2[kolStor + 1];
         line = transform.GetComponent<LineRenderer>();
         line.SetVertexCount(kolStor+1);
-        for (int i = 0; i < kolStor; i++)
+        for (int i = 0; i < kolStor+1; i++)
         {
-            poligonPoints[i] = new Vector2(Mathf.Cos(checkAngle * i), Mathf.Sin(checkAngle * i));
-            line.SetPosition(i, new Vector3(Mathf.Cos(checkAngle * i), Mathf.Sin(checkAngle * i), 0));
+            position[i] = new Vector2(Mathf.Cos(checkAngle * i) * _var, Mathf.Sin(checkAngle * i) * _var);
+            line.SetPosition(i, new Vector3(Mathf.Cos(checkAngle * i)*_var, Mathf.Sin(checkAngle * i)*_var, 0));
         }
 
-        line.SetPosition(kolStor,poligonPoints[0]);
-
+        line.SetPosition(kolStor, position[0]);
+        coll = gameObject.AddComponent<PolygonCollider2D>();
+        coll.SetPath(0, position);        
+        centrMass = coll.bounds.center;
+        addPoligon();
         
     }
+
+    void addPoligon()
+    {
+        Vector2 [] poligonPosition=new Vector2[position.Length*2];
+        float varCell=2f;
+
+        for (int i = 0; i < position.Length; i++)
+        {
+            poligonPosition[i] = new Vector2(position[i].x * varCell, position[i].y*varCell);
+            print(poligonPosition[i]);
+            poligonPosition[poligonPosition.Length-i-1] = new Vector2(position[i].x/varCell, position[i].y/varCell);
+            print(poligonPosition[poligonPosition.Length - i - 1]);            
+        }
+        cell = gameObject.AddComponent<PolygonCollider2D>();
+        cell.SetPath(0, poligonPosition);        
+
+    }
+
 }
